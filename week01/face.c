722be911d0f2f0c11a5a5eb6725d808cc5198180
Pat Chambers <z5264081@cse.unsw.edu.au>
2024-05-27 19:09:54 +1000

    Added code files

---
+++ week01/face.c
@@ -0,0 +1,38 @@
+// The program prints two simple faces
+// Pat Chambers (z5264081), 27/5/24
+
+#include <stdio.h>
+
+int main(void) {
+
+    // Face 1:
+    //  ~ ~
+    //  0 0
+    //   o
+    //   -
+    printf("Face 1: \n");
+    printf("~ ~\n");
+    printf("o o\n");
+    printf(" o \n");
+    printf(" - \n");
+    
+    // Face 2:
+    //  ~ ~
+    //  0 0
+    //   o
+    //  \_/
+    printf("Face 2: \n");
+    printf(
+        "~ ~\n"
+        "0 0\n"
+        " o \n"
+        "\\_/ \n"
+    );
+
+    return 0;
+}
+
+
+
+
+
---
+++ week01/hello_world.c
@@ -0,0 +1,11 @@
+// Basic Hello, World! program
+// Pat Chambers (z5264081), 27/5/24
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello, World!\n");
+    
+    return 0;
+}
